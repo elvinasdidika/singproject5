d888c54cdee10e8503c02aea3b0bf8db267bdb48
elvinasd <42936456+elvinasdidika@users.noreply.github.com>
2023-05-15 12:05:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('tenantButton').onclick = function () {
+    location.href = 'tenant_registration.html';
+};
+
+document.getElementById('landlordButton').onclick = function () {
+    location.href = 'landlord_registration.html';
+};
